239927a92a449690f3012b6f0961ca8739f3d799
mercy vincent <mercydanke@gmail.com>
2025-07-29 13:38:56 +0100

    feat(message): implement reply-to-message functionality with validation and media support

--- src/module/v1/chat/chat.module.ts
+++ src/module/v1/chat/chat.module.ts
@@ -1,50 +1,41 @@
-// import { Module } from '@nestjs/common';
-// import { ChatService } from './services/chat.service';
-// import { ChatController } from './controllers/chat.controller';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { Chat, ChatSchema } from './schema/chat.schema';
-// import { Message, MessageSchema } from './schema/message.schema';
-// import { RepositoryModule } from '../repository/repository.module';
-// import { MessageController } from './controllers/message.controller';
-// import { MessageService } from './services/message.service';
-// import { ChatGateway } from './chat.gateway';
-// import { UserModule } from '../user/user.module';
-// import {
-//   MessageReaction,
-//   MessageReactionSchema,
-// } from './schema/message-reaction.schema';
-// import { MessageReactionController } from './controllers/message-reaction.controller';
-// import { MessageReactionService } from './services/message-reaction.services';
-// import { ActiveUserService } from './services/chat-active-user.service';
+import { Module } from '@nestjs/common';
+import { ChatService } from './services/chat.service';
+import { ChatController } from './controllers/chat.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Chat, ChatSchema } from './schema/chat.schema';
+import { Message, MessageSchema } from './schema/message.schema';
+import { RepositoryModule } from '../repository/repository.module';
+import { MessageController } from './controllers/message.controller';
+import { MessageService } from './services/message.service';
+import { ChatGateway } from './chat.gateway';
+import { UserModule } from '../user/user.module';
+import { ActiveUserService } from './services/chat-active-user.service';
+import { PinataService } from 'src/common/utils/pinata.util';
 
-// @Module({
-//   imports: [
-//     MongooseModule.forFeature([
-//       {
-//         name: Chat.name,
-//         schema: ChatSchema,
-//       },
-//       {
-//         name: Message.name,
-//         schema: MessageSchema,
-//       },
-//       {
-//         name: MessageReaction.name,
-//         schema: MessageReactionSchema,
-//       },
-//     ]),
-//     RepositoryModule,
-//     UserModule,
-//     // NotificationModule,
-//   ],
-//   controllers: [ChatController, MessageController, MessageReactionController],
-//   providers: [
-//     ChatService,
-//     MessageService,
-//     ChatGateway,
-//     MessageReactionService,
-//     ActiveUserService,
-//   ],
-//   exports: [ChatService, MessageService, MessageReactionService],
-// })
-// export class ChatModule {}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Chat.name,
+        schema: ChatSchema,
+      },
+      {
+        name: Message.name,
+        schema: MessageSchema,
+      },
+    ]),
+    RepositoryModule,
+    UserModule,
+    // NotificationModule,
+  ],
+  controllers: [ChatController, MessageController],
+  providers: [
+    ChatService,
+    MessageService,
+    ChatGateway,
+    ActiveUserService,
+    PinataService,
+  ],
+  exports: [ChatService, MessageService, PinataService],
+})
+export class ChatModule {}
--- src/module/v1/chat/services/chat-active-user.service.ts
+++ src/module/v1/chat/services/chat-active-user.service.ts
@@ -1,32 +1,32 @@
-// import { Injectable } from '@nestjs/common';
-// import { CACHE_KEYS } from 'src/common/constants/cache.constant';
-// import { CacheHelperUtil } from 'src/common/utils/cache-helper.util';
+import { Injectable } from '@nestjs/common';
+import { CACHE_KEYS } from 'src/common/constants/cache.constant';
+import { CacheHelperUtil } from 'src/common/utils/cache-helper.util';
 
-// @Injectable()
-// export class ActiveUserService {
-//   async getActiveUser(userId: string) {
-//     const user = await CacheHelperUtil.getCache(CACHE_KEYS.ACTIVE_USERS);
-//     return user[userId];
-//   }
+@Injectable()
+export class ActiveUserService {
+  async getActiveUser(userId: string) {
+    const user = await CacheHelperUtil.getCache(CACHE_KEYS.ACTIVE_USERS);
+    return user[userId];
+  }
 
-//   async updateActiveUser(userId: string, socketId: string, isActive: boolean) {
-//     const user =
-//       (await CacheHelperUtil.getCache(CACHE_KEYS.ACTIVE_USERS)) || {};
+  async updateActiveUser(userId: string, socketId: string, isActive: boolean) {
+    const user =
+      (await CacheHelperUtil.getCache(CACHE_KEYS.ACTIVE_USERS)) || {};
 
-//     // If the user doesn't exist in the cache, create a new one, otherwise update the user
-//     user[userId] = {
-//       userId,
-//       socketId,
-//       isActive,
-//     };
+    // If the user doesn't exist in the cache, create a new one, otherwise update the user
+    user[userId] = {
+      userId,
+      socketId,
+      isActive,
+    };
 
-//     return CacheHelperUtil.setCache(CACHE_KEYS.ACTIVE_USERS, user);
-//   }
+    return CacheHelperUtil.setCache(CACHE_KEYS.ACTIVE_USERS, user);
+  }
 
-//   async removeActiveUser(userId: string) {
-//     const user = await CacheHelperUtil.getCache(CACHE_KEYS.ACTIVE_USERS);
-//     if (!user) return;
-//     delete user[userId];
-//     return CacheHelperUtil.setCache(CACHE_KEYS.ACTIVE_USERS, user);
-//   }
-// }
+  async removeActiveUser(userId: string) {
+    const user = await CacheHelperUtil.getCache(CACHE_KEYS.ACTIVE_USERS);
+    if (!user) return;
+    delete user[userId];
+    return CacheHelperUtil.setCache(CACHE_KEYS.ACTIVE_USERS, user);
+  }
+}
